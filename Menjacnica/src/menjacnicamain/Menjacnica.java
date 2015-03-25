d6a3cceaaa5f5459265c2958619ad168ab9b3424
petarjelenkovic <pera25blok@gmail.com>
2015-03-25 22:04:02 +0100

    kreirana klasa menjacnica koja implementira interfejs sa praznim metodama

---
+++ Menjacnica/src/menjacnicamain/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnicamain;
+
+import java.util.GregorianCalendar;
+
+import interfejsmenjacnica.MenjacnicaInter;
+
+public class Menjacnica implements MenjacnicaInter{
+
+	public void unesiKurs(String nazivValute, String skraceni,
+			GregorianCalendar datum, int srednji, int prodajni, int kupovni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obrisiKurs(String nazivValute, String skraceni,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Kurs pronadjiIVrati(String nazivValute, String skraceni,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
