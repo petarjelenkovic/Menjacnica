ba2c1cb3ab57f2faae2673541a39f73c1e3be0bb
petarjelenkovic <pera25blok@gmail.com>
2015-03-27 23:59:38 +0100

    Implementirana metoda za unos kursa dateToString napravljen public

--- Menjacnica/src/menjacnicamain/Menjacnica.java
+++ Menjacnica/src/menjacnicamain/Menjacnica.java
@@ -1,14 +1,25 @@
 package menjacnicamain;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejsmenjacnica.MenjacnicaInter;
 
 public class Menjacnica implements MenjacnicaInter{
+	
+	public LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
 
 	public void unesiKurs(String nazivValute, String skraceni,
 			GregorianCalendar datum, int srednji, int prodajni, int kupovni) {
-		// TODO Auto-generated method stub
+		Kurs kurs = new Kurs();
+		kurs.setDatumKursa(datum);
+		kurs.setNazivValute(nazivValute);
+		kurs.setSkraceniNaziv(skraceni);
+		kurs.setProdajniKurs(prodajni);
+		kurs.setSkraceniNaziv(skraceni);
+		kurs.setProdajniKurs(prodajni);
+		
+		kursevi.add(kurs);
 		
 	}
 
